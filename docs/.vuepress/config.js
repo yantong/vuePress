a2ae7c2c8d4451cc08c05bdb7f839e2b73bab044
0027020705 <yan.tong@iwhalecloud.com>
2022-04-06 19:49:26 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: 'Leaf',
+    description: 'Leaf Page',
+    base: '/leaf/',
+    theme: 'reco',
+    themeConfig: {
+      logo: 'leaf.png',
+      search: false,
+    }
+  }

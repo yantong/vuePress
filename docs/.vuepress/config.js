d3e68ef87a594060a7a77c47c49f7cf04b2da420
0027020705 <yan.tong@iwhalecloud.com>
2022-04-06 21:06:21 +0800

    modify

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,12 @@
 module.exports = {
-    title: 'Leaf',
-    description: 'Leaf Page',
-    base: '/leaf/',
-    theme: 'reco',
-    themeConfig: {
-      logo: 'leaf.png',
-      search: false,
-    }
+  title: 'Leaf',
+  description: 'Leaf Page',
+  base: '/leaf/',
+  theme: 'reco',
+  themeConfig: {
+    logo: '/leaf.png',
+    search: false,
+    type: 'blog',
+    authorAvatar: '/leaf.png',
   }
+}
---
+++ docs/index.md
@@ -0,0 +1,14 @@
+---
+home: true
+bgImage: '/bg.jpg'
+heroText:  null
+tagline: null
+actionText: null
+footer: MIT Licensed | Copyright © 2018-present Evan You˝
+---
+
+<style>
+    .footer-wrapper.footer {
+        display: none
+    }
+</style>
